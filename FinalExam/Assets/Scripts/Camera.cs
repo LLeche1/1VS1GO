6f3dbb9d8d8a23cd8bf97ddb250d228f0a41ee5e
LLeche1 <enddl2560@naver.com>
2022-09-08 21:21:45 +0900

    0908:2121

--- FinalExam/Assets/Scripts/Camera.cs
+++ FinalExam/Assets/Scripts/Camera.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public Transform player;
-    public float distance = 16.0f;
-    public float height = 6.0f;
-    public float damping = 10.0f;
+    public float distance;
+    public float height;
 
     void Update()
     {
         Vector3 pos = player.position - (Vector3.forward * distance) + (Vector3.up * height);
-        gameObject.transform.position = Vector3.Slerp(player.position, pos, Time.deltaTime * damping);
-        gameObject.transform.LookAt(player.position + (Vector3.up * 2));
+        gameObject.transform.position = pos;
     }
 }
--- FinalExam/Assets/Scripts/Player.cs
+++ FinalExam/Assets/Scripts/Player.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float speed = 1;
-    void Awake()
-    {
+    public float speed;
+    private float hAxis;
+    private float vAxis;
+    private float jDown;
+    private Vector3 moveDir;
+    private bool isJump;
 
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed * Vector3.forward * Time.deltaTime);
+        GetInput();
+        Move();
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
+        jDown = Input.GetAxis("Jump");
+    }
+
+    void Move()
+    {
+        gameObject.transform.Translate(new Vector3(hAxis, jAxis, vAxis) * speed * Time.deltaTime);
+        //moveDir = (Vector3.forward * vAxis + Vector3.right * hAxis).normalized;
+        //gameObject.transform.Translate(moveDir * speed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            isJump = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isJump = false;
+        }
     }
 }
