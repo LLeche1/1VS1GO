c886e73da613e32217ff99e33db2ec35002ec7fc
LLeche1 <enddl2560@naver.com>
2022-09-16 02:10:23 +0900

    0916:0210

--- FinalExam/Assets/Scripts/LobbyManager.cs
+++ FinalExam/Assets/Scripts/LobbyManager.cs
@@ -10,15 +10,18 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 {
     public InputField inputID; // 닉네임 입력받는 곳.
     public InputField inputPW;
-    public Text connectionInfoText; // 네트워크 정보를 표시할 텍스트
+    public Text connectInfo; // 네트워크 정보를 표시할 텍스트
     public Text currentPlayerCount; // 현재 서버에 접속된 인원
     public Button joinBtn; // 룸 접속 버튼
     public Button outBtn;
     public Button startBtn;
     public GameObject sign;
     public GameObject room;
+    public GameObject set;
+    public Toggle remember;
     private PhotonView PV;
     private string gameVersion = "1"; // 게임 버전
+    private string lastCanvas;
 
     void Awake()
     {
@@ -29,13 +32,13 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     // 게임 실행과 동시에 마스터 서버 접속 시도
     void Start()
     {
-        Load();
+        Load(remember);
         // 접속에 필요한 정보(게임 버전) 설정
         PhotonNetwork.GameVersion = gameVersion;
         // 설정한 정보를 가지고 마스터 서버 접속 시도
         PhotonNetwork.ConnectUsingSettings();
         // 접속을 시도 중임을 텍스트로 표시
-        connectionInfoText.text = "서버에 접속중...";
+        connectInfo.text = "서버에 접속중...";
     }
 
     // 마스터 서버 접속 성공시 자동 실행
@@ -44,14 +47,14 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         // 룸 접속 버튼을 활성화
         joinBtn.interactable = true;
         // 접속 정보 표시
-        connectionInfoText.text = "온라인";
+        connectInfo.text = "온라인";
     }
 
     // 마스터 서버 접속 실패시 자동 실행
     public override void OnDisconnected(DisconnectCause cause)
     {
         // 접속 정보 표시
-        connectionInfoText.text = "오프라인 : 서버와 연결되지 않음\n접속 재시도 중...";
+        connectInfo.text = "오프라인 : 서버와 연결되지 않음\n접속 재시도 중...";
 
         // 마스터 서버로의 재접속 시도
         PhotonNetwork.ConnectUsingSettings();
@@ -71,13 +74,13 @@ public class LobbyManager : MonoBehaviourPunCallbacks
             if (PhotonNetwork.IsConnected)
             {
                 // 룸 접속 실행
-                connectionInfoText.text = "매칭 중...";
+                connectInfo.text = "매칭 중...";
                 PhotonNetwork.JoinRandomRoom();
             }
             else
             {
                 // 마스터 서버에 접속중이 아니라면, 마스터 서버에 접속 시도
-                connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음\n접속 재시도 중...";
+                connectInfo.text = "오프라인 : 마스터 서버와 연결되지 않음\n접속 재시도 중...";
                 // 마스터 서버로의 재접속 시도
                 PhotonNetwork.ConnectUsingSettings();
             }
@@ -88,7 +91,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         // 접속 상태 표시
-        connectionInfoText.text = "방 생성 중...";
+        connectInfo.text = "방 생성 중...";
         // 최대 4명을 수용 가능한 빈방을 생성
         PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
     }
@@ -100,7 +103,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         room.SetActive(true);
         UpdatePlayerCounts();
         // 접속 상태 표시
-        connectionInfoText.text = "방 참가 성공";
+        connectInfo.text = "방 참가 성공";
         outBtn.interactable = true;
         if (photonView.IsMine)
         {
@@ -110,7 +113,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     private void UpdatePlayerCounts()
     {
-        if(connectionInfoText.text != "방 참가 성공")
+        if(connectInfo.text != "방 참가 성공")
         {
             currentPlayerCount.text = "";
         }
@@ -141,27 +144,78 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         }
     }
 
-    public void Remebmer(bool isOn)
+    public void Remember(Toggle toggle)
     {
-        if (isOn)
+        if (toggle.isOn)
         {
             PlayerPrefs.SetString("ID", inputID.text);
             PlayerPrefs.SetString("PW", inputPW.text);
+            PlayerPrefs.SetInt("IsOn", 1);
         }
-        else if (!isOn)
+        else
         {
             PlayerPrefs.DeleteAll();
         }
+
     }
 
-    void Load()
+    public void Back()
+    {
+        if (lastCanvas == "sign")
+        {
+            set.SetActive(false);
+            sign.SetActive(true);
+            connectInfo.enabled = true;
+        }
+        else if (lastCanvas == "room")
+        {
+            set.SetActive(false);
+            room.SetActive(true);
+            connectInfo.enabled = true;
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void Load(Toggle toggle)
     {
         inputID.text = PlayerPrefs.GetString("ID");
         inputPW.text = PlayerPrefs.GetString("PW");
+        if(PlayerPrefs.GetInt("IsOn") == 1)
+        {
+            toggle.isOn = true;
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+    }
+
+    void Set()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (sign.activeSelf == true)
+            {
+                sign.SetActive(false);
+                lastCanvas = "sign";
+            }
+            else if (room.activeSelf == true)
+            {
+                room.SetActive(false);
+                lastCanvas = "room";
+            }
+            set.SetActive(true);
+            connectInfo.enabled = false;
+        }
     }
 
     void Update()
     {
         UpdatePlayerCounts();
+        Set();
     }
 }
