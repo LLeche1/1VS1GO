b38c70231986e48bcea6bc9d20a58e028dedf2de
LLeche1 <enddl2560@naver.com>
2022-09-19 16:09:15 +0900

    0919:1609

--- FinalExam/Assets/Scripts/LobbyManager.cs
+++ FinalExam/Assets/Scripts/LobbyManager.cs
@@ -5,21 +5,23 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
 
-// 마스터(매치 메이킹) 서버와 룸 접속을 담당
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
     public InputField inputID;
     public InputField inputPW;
     public InputField inputRoom;
     public Text connectInfo;
-    public Text lobbyInfo;
     public Text roomInfo;
-    public Text currentPlayerCount;
+    public Text roomInfo2;
     public Text playerList;
-    public Button joinBtn; // 룸 접속 버튼
+    public Button joinBtn;
     public Button startBtn;
     public Button outBtn;
-    public GameObject sign;
+    public Button previousBtn;
+    public Button nextBtn;
+    public Button[] cellBtn;
+    public GameObject login;
+    public GameObject loginError;
     public GameObject lobby;
     public GameObject room;
     public GameObject roomNum;
@@ -29,6 +31,9 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     private PhotonView PV;
     private string gameVersion = "1";
     private string lastCanvas;
+    private int currentPage = 1;
+    private int maxPage;
+    private int multiple;
     List<RoomInfo> roomList = new List<RoomInfo>();
 
     void Awake()
@@ -39,74 +44,65 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         PV = GetComponent<PhotonView>();
     }
 
-    // 게임 실행과 동시에 마스터 서버 접속 시도
     void Start()
     {
         Load(remember);
-        // 접속에 필요한 정보(게임 버전) 설정
         PhotonNetwork.GameVersion = gameVersion;
-        // 설정한 정보를 가지고 마스터 서버 접속 시도
         PhotonNetwork.ConnectUsingSettings();
-        // 접속을 시도 중임을 텍스트로 표시
         connectInfo.text = "서버에 접속중...";
     }
 
-    // 마스터 서버 접속 성공시 자동 실행
     public override void OnConnectedToMaster()
     {
-        // 룸 접속 버튼을 활성화
         joinBtn.interactable = true;
-        // 접속 정보 표시
         connectInfo.text = "온라인";
     }
 
-    // 마스터 서버 접속 실패시 자동 실행
     public override void OnDisconnected(DisconnectCause cause)
     {
-        // 접속 정보 표시
         connectInfo.text = "오프라인 : 서버와 연결되지 않음\n접속 재시도 중...";
-
-        // 마스터 서버로의 재접속 시도
         PhotonNetwork.ConnectUsingSettings();
     }
 
-    // 룸 접속 시도
     public void Connect()
     {
-        string nick = inputID.text;
-        if (nick != "")
+        string id = inputID.text;
+        string pw = inputPW.text;
+        if (id != "" && pw != "")
         {
-            // 중복 접속 시도를 막기 위해, 접속 버튼 잠시 비활성화
             joinBtn.interactable = false;
 
-            // 마스터 서버에 접속중이라면
             if (PhotonNetwork.IsConnected)
             {
-                // 룸 접속 실행
                 connectInfo.text = "매칭 중...";
                 PhotonNetwork.JoinLobby();
-                sign.SetActive(false);
-                lobby.SetActive(true);
-                PhotonNetwork.LocalPlayer.NickName = nick;
-                //PhotonNetwork.JoinRandomRoom();
+                PhotonNetwork.LocalPlayer.NickName = id;
             }
             else
             {
-                // 마스터 서버에 접속중이 아니라면, 마스터 서버에 접속 시도
                 connectInfo.text = "오프라인 : 마스터 서버와 연결되지 않음\n접속 재시도 중...";
-                // 마스터 서버로의 재접속 시도
                 PhotonNetwork.ConnectUsingSettings();
             }
         }
+        else
+        {
+            login.SetActive(false);
+            loginError.SetActive(true);
+        }
     }
 
-    // (빈 방이 없어)랜덤 룸 참가에 실패한 경우 자동 실행
+    public override void OnJoinedLobby()
+    {
+        login.SetActive(false);
+        lobby.SetActive(true);
+        roomList.Clear();
+    }
+
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         CreateRoom();
     }
 
-    // 룸에 참가 완료된 경우 자동 실행
     public override void OnJoinedRoom()
     {
         lobby.SetActive(false);
@@ -120,7 +116,6 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         }
     }
 
-    // 룸 생성 실패시 자동 실행
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         roomNum.SetActive(false);
@@ -137,16 +132,52 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         RoomRenewal();
     }
 
+    public override void OnRoomListUpdate(List<RoomInfo> room)
+    {
+        int roomCount = room.Count;
+        for(int i = 0; i < roomCount; i++)
+        {
+            if (!room[i].RemovedFromList)
+            {
+                if (!roomList.Contains(room[i]))
+                {
+                    roomList.Add(room[i]);
+                }
+                else
+                {
+                    roomList[roomList.IndexOf(room[i])] = room[i];
+                }
+            }
+            else if (roomList.IndexOf(room[i]) != -1)
+            {
+                roomList.RemoveAt(roomList.IndexOf(room[i]));
+            }
+        }
+        roomListRenewal();
+    }
+
     public void DisconnectBtn()
     {
         PhotonNetwork.Disconnect();
         lobby.SetActive(false);
-        sign.SetActive(true);
+        login.SetActive(true);
     }
 
-    public void RoomListBtn()
+    public void RoomListBtn(int num)
     {
-
+        if (num == -2)
+        {
+            --currentPage;
+        }
+        else if(num == -1)
+        {
+            ++currentPage;
+        }
+        else
+        {
+            PhotonNetwork.JoinRoom(roomList[multiple + num].Name);
+        }
+        roomListRenewal();
     }
 
     public void RoomNum()
@@ -169,8 +200,6 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            // 목표 인원 수 채웠으면, 맵 이동을 한다. 권한은 마스터 클라이언트만.
-            // PhotonNetwork.AutomaticallySyncScene = true; 를 해줬어야 방에 접속한 인원이 모두 이동함.
             /*if (PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers)
             {
                 PhotonNetwork.LoadLevel("InGame 1");
@@ -204,7 +233,12 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     public void Back()
     {
-        if (lastCanvas == "roomNum")
+        if(lastCanvas == "loginError")
+        {
+            loginError.SetActive(false);
+            login.SetActive(true);
+        }
+        else if (lastCanvas == "roomNum")
         {
             roomNum.SetActive(false);
             lobby.SetActive(true);
@@ -218,10 +252,15 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     public void SetBack()
     {
-        if (lastCanvas == "sign")
+        if (lastCanvas == "login")
         {
             set.SetActive(false);
-            sign.SetActive(true);
+            login.SetActive(true);
+        }
+        else if (lastCanvas == "loginError")
+        {
+            set.SetActive(false);
+            loginError.SetActive(true);
         }
         else if (lastCanvas == "lobby")
         {
@@ -250,6 +289,22 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         Application.Quit();
     }
 
+    void roomListRenewal()
+    {
+        maxPage = (roomList.Count % cellBtn.Length == 0) ? roomList.Count / cellBtn.Length : roomList.Count / cellBtn.Length + 1;
+
+        previousBtn.interactable = (currentPage <= 1) ? false : true;
+        nextBtn.interactable = (currentPage >= maxPage) ? false : true;
+
+        multiple = (currentPage - 1) * cellBtn.Length;
+        for (int i = 0; i < cellBtn.Length; i++)
+        {
+            cellBtn[i].interactable = (multiple + i < roomList.Count) ? true : false;
+            cellBtn[i].transform.GetChild(0).GetComponent<Text>().text = (multiple + i < roomList.Count) ? roomList[multiple + i].Name : "";
+            cellBtn[i].transform.GetChild(1).GetComponent<Text>().text = (multiple + i < roomList.Count) ? roomList[multiple + i].PlayerCount + "/" + roomList[multiple + i].MaxPlayers : "";
+        }
+    }
+
     void Load(Toggle toggle)
     {
         inputID.text = PlayerPrefs.GetString("ID");
@@ -268,9 +323,13 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (lastCanvas == "sign")
+            if (lastCanvas == "login")
             {
-                sign.SetActive(false);
+                login.SetActive(false);
+            }
+            else if (lastCanvas == "loginError")
+            {
+                loginError.SetActive(false);
             }
             else if (lastCanvas == "lobby")
             {
@@ -294,9 +353,13 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     void LastCanvas()
     {
-        if (sign.activeSelf == true)
+        if (login.activeSelf == true)
         {
-            lastCanvas = "sign";
+            lastCanvas = "login";
+        }
+        else if (loginError.activeSelf == true)
+        {
+            lastCanvas = "loginError";
         }
         else if (lobby.activeSelf == true)
         {
@@ -324,13 +387,12 @@ public class LobbyManager : MonoBehaviourPunCallbacks
             playerList.text += PhotonNetwork.PlayerList[i].NickName + ((i + 1 == PhotonNetwork.PlayerList.Length) ? "" : "\n\n ");
         }
         roomInfo.text = "방 번호 : " + PhotonNetwork.CurrentRoom.Name;
-        //" 현재인원 : " + PhotonNetwork.CurrentRoom.PlayerCount + " 최대인원 : " + PhotonNetwork.CurrentRoom.MaxPlayers;
+        roomInfo2.text = PhotonNetwork.CurrentRoom.PlayerCount + " / " + PhotonNetwork.CurrentRoom.MaxPlayers;
     }
 
     void Update()
     {
         Set();
         LastCanvas();
-        //lobbyInfo = (PhotonNetwork.CountOfPlayers - PhotonNetwork.CountOfPlayersInRooms) + "로비 / " + PhotonNetwork.CountOfPlayers + "접속";
     }
 }
