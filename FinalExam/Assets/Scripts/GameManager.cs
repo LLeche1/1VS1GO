fe7aa65717b3222c49806444ed02953639ae4ee3
Jungbo77 <kimsx77@naver.com>
2022-09-25 00:35:11 +0900

    0925:0135 - JH

--- FinalExam/Assets/Scripts/GameManager.cs
+++ FinalExam/Assets/Scripts/GameManager.cs
@@ -76,11 +76,12 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
     IEnumerator RecallCoolTime(GameObject player)
     {
         player.active = false;
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(5.0f);
         player.transform.position = new Vector3(Random.Range(-4f, 4f), player.transform.position.y, wagon.transform.position.z - 20f);
         player.GetComponent<Player>().playerHp = 100;
         player.GetComponent<Player>().isDead = false;
         player.active = true;
+        player.GetComponent<Player>().HpBar();
     }
 
     [PunRPC]
