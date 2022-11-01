358a7e9e7b6aa8c24ffe3faa38f86980dee778cc
Jungbo77 <kimsx77@naver.com>
2022-11-02 00:24:00 +0900

    1102:0023

--- FinalExam/Assets/Scripts/RunningGame.cs
+++ FinalExam/Assets/Scripts/RunningGame.cs
@@ -6,62 +6,120 @@ using Photon.Realtime;
 
 public class RunningGame : MonoBehaviourPunCallbacks
 {
+    GameManager gameManager;
+    GameObject maps;
+    GameObject frontLineBoard;
+    Vector3 preBoardPos;
+    List<GameObject> TrackList;
+
+    public GameObject chariot;
+    private GameObject chariotObj;
+    float chariotSpeed = 1f;
+    const int chariotGenTime = 3;
+
     const int trackNum = 10;
     const int trackLength = 16;
+    const int rTrackPatternCount = 6;
     private int[] randNumArray;
     public GameObject firstTrack;
     public GameObject endTrack;
-    public GameObject[] tracks;
+    public GameObject runningTrack;
     PhotonView PV;
+
+
 
     void Start()
     {
+        TrackList = new List<GameObject>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        maps = transform.Find("Maps").gameObject;
         PV = GetComponent<PhotonView>();
         FirstEndTrackSet();
-        if (PhotonNetwork.IsMasterClient)
+        StartCoroutine(ChariotSpawn());
+    }
+
+    void Update()
+    {
+        TrackGenerate();
+        ChariotAcceleration();
+        RemovePastTrack();
+    }
+
+    void TrackGenerate()
+    {
+        foreach (var player in gameManager.players)
         {
-            randNumArray = RandNumGenerate();
-            PV.RPC("TrackNumSync", RpcTarget.All, randNumArray);
-            //PV.RPC("BoardSetting", RpcTarget.All);
+            if (TrackList[TrackList.Count - 1].transform.position.z - player.transform.position.z < 60f)
+            {
+                int randPatternNum = Random.Range(0, rTrackPatternCount);
+                preBoardPos += new Vector3(0f, 0f, 20f);
+                PV.RPC(nameof(RunningTrackCreateSync), RpcTarget.All, preBoardPos, randPatternNum);
+            }
         }
-        BoardSetting();
     }
 
     void FirstEndTrackSet()
     {
-        GameObject fT = Instantiate(firstTrack, this.transform);
+        GameObject fT = Instantiate(firstTrack);
+        TrackList.Add(fT);
         fT.transform.position = Vector3.zero;
-        fT.transform.parent = GameObject.Find("InGame").transform.GetChild(1).transform;
+        fT.transform.parent = maps.transform;
+    }
+    [PunRPC]
+    void RunningTrackCreateSync(Vector3 initPos, int randNum)
+    {
+        GameObject obj = Instantiate(runningTrack, maps.transform);
+        TrackList.Add(obj);
+        obj.transform.position = initPos;
+        obj.transform.Find("ObstaclePattern").GetChild(randNum).gameObject.SetActive(true);
+    }
+    
+    void RunningTrackRandPattern()
+    {
 
-        GameObject eT = Instantiate(endTrack, this.transform);
-        eT.transform.position = new Vector3(0f, 0f, trackLength * (trackNum + 1));
-        eT.transform.parent = GameObject.Find("InGame").transform.GetChild(1).transform;
     }
 
-    [PunRPC]
-    void BoardSetting()
+    IEnumerator ChariotSpawn()
     {
-        for (int i = 1; i <= trackNum; i++)
+        float timer = 0;
+        while(timer < chariotGenTime)
         {
-            int randVal = Random.Range(0, tracks.Length);
-            GameObject track = Instantiate(tracks[randNumArray[i - 1]]);
-            track.transform.position = new Vector3(0f, 0f, trackLength * i);
-            track.transform.parent = GameObject.Find("InGame").transform.GetChild(1).transform;
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+        }
+        PV.RPC(nameof(ChariotCreateSync), RpcTarget.All);
+        yield return null;
+    }
+    [PunRPC]
+    void ChariotCreateSync()
+    {
+        GameObject obj = Instantiate(chariot, this.transform);
+        obj.transform.position = new Vector3(-4f, 0f, 0f);
+        obj.transform.localScale = new Vector3(4f, 2.5f, 2.5f);
+        chariotObj = obj;
+    }
+    
+    void ChariotAcceleration()
+    {
+        if(chariotObj != null)
+        {
+            chariotObj.transform.Translate(Vector3.forward * chariotSpeed * Time.deltaTime);
+            chariotSpeed *= 1.001f;
         }
     }
-    int[] RandNumGenerate()
-    {
-        int[] numArray = new int[trackNum];
-        for (int i = 0; i < trackNum; i++)
-        {
-            numArray[i] = Random.Range(0, tracks.Length);
-        }
-        return numArray;
-    }
 
-    [PunRPC]
-    void TrackNumSync(int[] numArray)
+    void RemovePastTrack()
     {
-        randNumArray = numArray;
+        if (chariotObj != null)
+        {
+            foreach (var track in TrackList)
+            {
+                if (chariotObj.transform.position.z > track.transform.position.z + 20)
+                {
+                    TrackList.Remove(track);
+                    Destroy(track.gameObject);
+                }
+            }
+        }
     }
 }
