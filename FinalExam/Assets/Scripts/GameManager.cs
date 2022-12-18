d285150bf9b79d78786a9a65c0f406939a2ee534
Jungbo77 <kimsx77@naver.com>
2022-12-18 22:33:48 +0900

    1218:2233

--- FinalExam/Assets/Scripts/GameManager.cs
+++ FinalExam/Assets/Scripts/GameManager.cs
@@ -236,14 +236,18 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     void RoundStart()
     {
-        isRandom = true;
+        /*isRandom = true;
         int i = Random.Range(0, randomNum);
         while (i == beforeMapIndex)
         {
             i = Random.Range(0, randomNum);
         }
         random = randomList[i];
-        beforeMapIndex = i;
+        beforeMapIndex = i;*/
+        if (!(beforeMapIndex > 3))
+        {
+            random = beforeMapIndex + 1;
+        }
         PV.RPC("RandomMap", RpcTarget.All, random);
     }
 
